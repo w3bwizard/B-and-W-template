2147d7059e33bb8b68ab19a200a84897c409883c
w3bwizard <w3bwiz4rd@gmail.com>
2021-01-20 23:06:03 +0200

    добавил меню и основной адаптив

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    let button = $('#burger');
+    let nav = $('#topnav');
+    let link = $('.topnav__link');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    link.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    $(document).click(function (event) {
+        if ($(event.target).closest("#burger").length)
+            return;
+        if ($(event.target).closest("#topnav").length)
+            return;
+        nav.removeClass('active');
+        button.removeClass('active');
+    });
+
+    function toggleMenu() {
+        nav.toggleClass('active');
+        button.toggleClass('active');
+    }
+});
